3c8b7c143b7251757d7fc90d5ba4f7966edc92bc
Isaac Woods <isaacwoods.home@gmail.com>
2020-09-27 23:15:12 +0100

    Compact address type

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,46 @@
 #![no_std]
+
+use bit_field::BitField;
+use core::{fmt, marker::PhantomData};
+
+/// PCIe supports 65536 segments, each with 256 buses, each with 32 slots, each with 8 possible functions. We cram this into a `u32`:
+///
+/// 32                              16               8         3      0
+///  +-------------------------------+---------------+---------+------+
+///  |            segment            |      bus      | device  | func |
+///  +-------------------------------+---------------+---------+------+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug, Default)]
+pub struct PciAddress(u32);
+
+impl PciAddress {
+    pub fn new(segment: u16, bus: u8, device: u8, function: u8) -> PciAddress {
+        let mut result = 0;
+        result.set_bits(0..3, function as u32);
+        result.set_bits(3..8, device as u32);
+        result.set_bits(8..16, bus as u32);
+        result.set_bits(16..32, segment as u32);
+        PciAddress(result)
+    }
+
+    pub fn segment(&self) -> u16 {
+        self.0.get_bits(16..32) as u16
+    }
+
+    pub fn bus(&self) -> u8 {
+        self.0.get_bits(8..16) as u8
+    }
+
+    pub fn device(&self) -> u8 {
+        self.0.get_bits(3..8) as u8
+    }
+
+    pub fn function(&self) -> u8 {
+        self.0.get_bits(0..3) as u8
+    }
+}
+
+impl fmt::Display for PciAddress {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02x}-{:02x}:{:02x}.{}", self.segment(), self.bus(), self.device(), self.function())
+    }
+}
