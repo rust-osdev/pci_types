144da347c6bf5bb7ada8da513f6b61ab3243326d
alesharikReserv@yandex.ru <alesharikReserv@yandex.ru>
2022-05-21 16:54:50 +0300

    Add capabilities and status register support

---
+++ src/capability/mod.rs
@@ -0,0 +1,134 @@
+use crate::{ConfigRegionAccess, PciAddress};
+use bit_field::BitField;
+use core::fmt::Formatter;
+
+mod msi;
+
+pub use msi::{MsiCapability, MultipleMessageSupport, TriggerMode};
+
+#[derive(Clone)]
+pub struct PciCapabilityAddress {
+    pub address: PciAddress,
+    pub offset: u16,
+}
+
+impl core::fmt::Debug for PciCapabilityAddress {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{}, offset: {:02x}", self.address, self.offset)
+    }
+}
+
+/// PCI capabilities
+#[derive(Clone, Debug)]
+pub enum PciCapability {
+    /// Power management capability, Cap ID = `0x01`
+    PowerManagement(PciCapabilityAddress),
+    /// Accelerated graphics port capability, Cap ID = `0x02`
+    AcceleratedGraphicsPort(PciCapabilityAddress),
+    /// Vital product data capability, Cap ID = `0x3`
+    VitalProductData(PciCapabilityAddress),
+    /// Slot identification capability, Cap ID = `0x04`
+    SlotIdentification(PciCapabilityAddress),
+    /// Message signalling interrupts capability, Cap ID = `0x05`
+    Msi(MsiCapability),
+    /// CompactPCI HotSwap capability, Cap ID = `0x06`
+    CompactPCIHotswap(PciCapabilityAddress),
+    /// PCI-X capability, Cap ID = `0x07`
+    PciX(PciCapabilityAddress),
+    /// HyperTransport capability, Cap ID = `0x08`
+    HyperTransport(PciCapabilityAddress),
+    /// Vendor-specific capability, Cap ID = `0x09`
+    Vendor(PciCapabilityAddress),
+    /// Debug port capability, Cap ID = `0x0A`
+    DebugPort(PciCapabilityAddress),
+    /// CompactPCI Central Resource Control capability, Cap ID = `0x0B`
+    CompactPCICentralResourceControl(PciCapabilityAddress),
+    /// PCI Standard Hot-Plug Controller capability, Cap ID = `0x0C`
+    PciHotPlugControl(PciCapabilityAddress),
+    /// Bridge subsystem vendor/device ID capability, Cap ID = `0x0D`
+    BridgeSubsystemVendorId(PciCapabilityAddress),
+    /// AGP Target PCI-PCI bridge capability, Cap ID = `0x0E`
+    AGP3(PciCapabilityAddress),
+    /// PCI Express capability, Cap ID = `0x10`
+    PciExpress(PciCapabilityAddress),
+    /// MSI-X capability, Cap ID = `0x11`
+    MsiX(PciCapabilityAddress),
+    /// Unknown capability
+    Unknown {
+        address: PciCapabilityAddress,
+        id: u8,
+    },
+}
+
+impl PciCapability {
+    fn parse(id: u8, address: PciCapabilityAddress, control: u16) -> Option<PciCapability> {
+        match id {
+            0x00 => None, // null capability
+            0x01 => Some(PciCapability::PowerManagement(address)),
+            0x02 => Some(PciCapability::AcceleratedGraphicsPort(address)),
+            0x03 => Some(PciCapability::VitalProductData(address)),
+            0x04 => Some(PciCapability::SlotIdentification(address)),
+            0x05 => Some(PciCapability::Msi(MsiCapability::new(address, control))),
+            0x06 => Some(PciCapability::CompactPCIHotswap(address)),
+            0x07 => Some(PciCapability::PciX(address)),
+            0x08 => Some(PciCapability::HyperTransport(address)),
+            0x09 => Some(PciCapability::Vendor(address)),
+            0x0A => Some(PciCapability::DebugPort(address)),
+            0x0B => Some(PciCapability::CompactPCICentralResourceControl(address)),
+            0x0C => Some(PciCapability::PciHotPlugControl(address)),
+            0x0D => Some(PciCapability::BridgeSubsystemVendorId(address)),
+            0x0E => Some(PciCapability::AGP3(address)),
+            0x10 => Some(PciCapability::PciExpress(address)),
+            0x11 => Some(PciCapability::MsiX(address)),
+            _ => Some(PciCapability::Unknown { address, id }),
+        }
+    }
+}
+
+pub struct CapabilityIterator<'a, T: ConfigRegionAccess> {
+    address: PciAddress,
+    offset: u16,
+    access: &'a T,
+}
+
+impl<'a, T: ConfigRegionAccess> CapabilityIterator<'a, T> {
+    pub(crate) fn new(
+        address: PciAddress,
+        offset: u16,
+        access: &'a T,
+    ) -> CapabilityIterator<'a, T> {
+        CapabilityIterator {
+            address,
+            offset,
+            access,
+        }
+    }
+}
+
+impl<'a, T: ConfigRegionAccess> Iterator for CapabilityIterator<'a, T> {
+    type Item = PciCapability;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.offset == 0 {
+                return None;
+            }
+            let data = unsafe { self.access.read(self.address, self.offset) };
+            let next_ptr = data.get_bits(8..16);
+            let id = data.get_bits(0..8);
+            let control = data.get_bits(16..32) as u16;
+            let cap = PciCapability::parse(
+                id as u8,
+                PciCapabilityAddress {
+                    address: self.address,
+                    offset: self.offset,
+                },
+                control,
+            );
+            self.offset = next_ptr as u16;
+            if let Some(cap) = cap {
+                return Some(cap);
+            }
+        }
+    }
+}
---
+++ src/register.rs
@@ -0,0 +1,128 @@
+use bit_field::BitField;
+use core::convert::TryFrom;
+use core::fmt::{Debug, Formatter};
+
+/// Slowest time that a device will assert DEVSEL# for any bus command except Configuration Space
+/// read and writes
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum DevselTiming {
+    Fast = 0x0,
+    Medium = 0x1,
+    Slow = 0x2,
+}
+
+impl TryFrom<u8> for DevselTiming {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x0 => Ok(DevselTiming::Fast),
+            0x1 => Ok(DevselTiming::Medium),
+            0x2 => Ok(DevselTiming::Slow),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+pub struct StatusRegister(u16);
+
+impl StatusRegister {
+    pub fn new(value: u16) -> Self {
+        StatusRegister(value)
+    }
+
+    /// Will be `true` whenever the device detects a parity error, even if parity error handling is disabled.
+    pub fn parity_error_detected(&self) -> bool {
+        self.0.get_bit(15)
+    }
+
+    /// Will be `true` whenever the device asserts SERR#.
+    pub fn signalled_system_error(&self) -> bool {
+        self.0.get_bit(14)
+    }
+
+    /// Will return `true`, by a master device, whenever its transaction
+    /// (except for Special Cycle transactions) is terminated with Master-Abort.
+    pub fn received_master_abort(&self) -> bool {
+        self.0.get_bit(13)
+    }
+
+    /// Will return `true`, by a master device, whenever its transaction is terminated with Target-Abort.
+    pub fn received_target_abort(&self) -> bool {
+        self.0.get_bit(12)
+    }
+
+    /// Will return `true` whenever a target device terminates a transaction with Target-Abort.
+    pub fn signalled_target_abort(&self) -> bool {
+        self.0.get_bit(11)
+    }
+
+    /// The slowest time that a device will assert DEVSEL# for any bus command except
+    /// Configuration Space read and writes.
+    ///
+    /// For PCIe always set to `Fast`
+    pub fn devsel_timing(&self) -> Result<DevselTiming, ()> {
+        let bits = self.0.get_bits(9..11);
+        DevselTiming::try_from(bits as u8)
+    }
+
+    /// This returns `true` only when the following conditions are met:
+    /// - The bus agent asserted PERR# on a read or observed an assertion of PERR# on a write
+    /// - the agent setting the bit acted as the bus master for the operation in which the error occurred
+    /// - bit 6 of the Command register (Parity Error Response bit) is set to 1.
+    pub fn master_data_parity_error(&self) -> bool {
+        self.0.get_bit(8)
+    }
+
+    /// If returns `true` the device can accept fast back-to-back transactions that are not from
+    /// the same agent; otherwise, transactions can only be accepted from the same agent.
+    ///
+    /// For PCIe always set to `false`
+    pub fn fast_back_to_back_capable(&self) -> bool {
+        self.0.get_bit(7)
+    }
+
+    /// If returns `true` the device is capable of running at 66 MHz; otherwise, the device runs at 33 MHz.
+    ///
+    /// For PCIe always set to `false`
+    pub fn capable_66mhz(&self) -> bool {
+        self.0.get_bit(5)
+    }
+
+    /// If returns `true` the device implements the pointer for a New Capabilities Linked list;
+    /// otherwise, the linked list is not available.
+    ///
+    /// For PCIe always set to `true`
+    pub fn has_capability_list(&self) -> bool {
+        self.0.get_bit(4)
+    }
+
+    /// Represents the state of the device's INTx# signal. If returns `true` and bit 10 of the
+    /// Command register (Interrupt Disable bit) is set to 0 the signal will be asserted;
+    /// otherwise, the signal will be ignored.
+    pub fn interrupt_status(&self) -> bool {
+        self.0.get_bit(3)
+    }
+}
+
+impl Debug for StatusRegister {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("StatusRegister")
+            .field("parity_error_detected", &self.parity_error_detected())
+            .field("signalled_system_error", &self.signalled_system_error())
+            .field("received_master_abort", &self.received_master_abort())
+            .field("received_target_abort", &self.received_target_abort())
+            .field("signalled_target_abort", &self.signalled_target_abort())
+            .field("devsel_timing", &self.devsel_timing())
+            .field("master_data_parity_error", &self.master_data_parity_error())
+            .field(
+                "fast_back_to_back_capable",
+                &self.fast_back_to_back_capable(),
+            )
+            .field("capable_66mhz", &self.capable_66mhz())
+            .field("has_capability_list", &self.has_capability_list())
+            .field("interrupt_status", &self.interrupt_status())
+            .finish()
+    }
+}
