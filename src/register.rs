7cde1c8fe3e458722db6cd1d14f3916a3dac60ea
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-06-04 16:23:41 +0200

    feat: make `CommandRegister` `#[repr(transparent)]`
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- src/register.rs
+++ src/register.rs
@@ -138,6 +138,7 @@ impl Debug for StatusRegister {
 }
 
 bitflags::bitflags! {
+    #[repr(transparent)]
     #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
     pub struct CommandRegister: u16 {
         const IO_ENABLE = 1 << 0;
